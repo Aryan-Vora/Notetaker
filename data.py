e2d38144116f882806ee220225a3bad7e16cb0c1
Aryan Vora <voraaryan1@gmail.com>
2023-10-16 22:39:12 -0700

    better prompt

--- data.py
+++ data.py
@@ -111,7 +111,7 @@ def summarize(document):
 
         summaries = []
         for chunk in chunks:
-            prompt = "Please summarize this: \n"
+            prompt = "Please take notes on this. They should be in bullet points and cover main ideas. Do not take too many notes. They should be short and concise, but also have the flexibility to be longer if need be. "
             summary = gpt3_completion(prompt + chunk)
             if summary.startswith("GPT-3 error:"):
                 continue
